20f358ed97ce15b51d5ad5a86d12d457bdeff992
qq <qq>
2017-07-24 21:43:46 +0200

    repozytorium ksiazek

---
+++ book.go
@@ -0,0 +1,27 @@
+package main
+
+type Book struct {
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	ISBN        string `json:"isbn"`
+	Description string `json:"description,omitempty"`
+}
+
+var books = map[string]Book{
+	"1": Book{Title: "pierwsza", Author: "pierwszy autor", ISBN: "isbn1", Description: "opeis1"},
+	"2": Book{Title: "druga", Author: "drugi autor", ISBN: "isbn2"},
+}
+
+func AllBooks() []Book {
+	values := make([]Book, len(books))
+	idx := 0
+	for _, book := range books {
+		values[idx] = book
+		idx++
+	}
+	return values
+}
+func GetBook(isbn string) (Book, bool) {
+	book, found := books[isbn]
+	return book, found
+}
